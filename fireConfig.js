d361c87a9df7de7a7379bb2f40d227c92fac40a3
Gustavo Ferreira <132668345+Ferreirxw@users.noreply.github.com>
2024-04-04 16:30:32 -0300

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCZv9T6kzd59pJWvPnOh6B5ELNBrYw_hbk",
+    authDomain: "crud-ferreirxw.firebaseapp.com",
+    databaseURL: "https://crud-ferreirxw-default-rtdb.firebaseio.com",
+    projectId: "crud-ferreirxw",
+    storageBucket: "crud-ferreirxw.appspot.com",
+    messagingSenderId: "958011735189",
+    appId: "1:958011735189:web:daa0d4ddc02562c5152f9c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
